9f2256a22a62fced7fd10f8b0906602b907ef4dc
Surat Adhikari1 <surajadkhari@gmail.com>
2020-06-07 18:20:46 +0545

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blueGrey,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ltb=1;
+  int rtb=1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ltb=Random().nextInt(6)+1;
+                });
+              },
+              child: Image.asset('images/dice$ltb.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+               setState(() {
+                 rtb=Random().nextInt(6)+1;
+               });
+              },
+              child: Image.asset('images/dice$rtb.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
